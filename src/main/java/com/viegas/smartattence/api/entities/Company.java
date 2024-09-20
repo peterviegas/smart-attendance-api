543f0a21458ba0ab3d52f4c569e5e9c862f6d917
Peter <peterviegas@outlook.com>
2024-09-20 22:01:20 +0100

    Created Entitie Company

--- src/main/java/com/viegas/smartattence/api/entities/Company.java
+++ src/main/java/com/viegas/smartattence/api/entities/Company.java
@@ -1,16 +1,121 @@
 package com.viegas.smartattence.api.entities;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "company")
 public class Company  implements Serializable {
 	
 private static final long serialVersionUID = 3960436649365666213L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
-	private String legalName;
-	private String cnpj;
-	private Date createDate;
-	private Date updateDate;
-	private List<Employee> employees;
 
+	@Column(name = "legalName", nullable = false)
+	private String legalName;
+	
+	@Column(name = "cnpj", nullable = false)
+	private String cnpj;
+	
+	@Column(name = "createDate", nullable = false)
+	private Date createDate;
+	
+	@Column(name = "updateDate", nullable = false)
+	private Date updateDate;
+	
+	@OneToMany(mappedBy = "employee", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	private List<Employee> employees;
+	
+	public Company() {
+		super();
+	}
+	
+	public Company(Long id, String legalName, String cnpj, Date createDate, Date updateDate) {
+		this.id = id;
+		this.legalName = legalName;
+		this.cnpj = cnpj;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getLegalName() {
+		return legalName;
+	}
+
+	public void setLegalName(String legalName) {
+		this.legalName = legalName;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public List<Employee> getEmployees() {
+		return employees;
+	}
+
+	public void setEmployees(List<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	@PreUpdate
+    public void preUpdate() {
+		updateDate = new Date();
+    }
+     
+    @PrePersist
+    public void prePersist() {
+        final Date atual = new Date();
+        createDate = atual;
+        updateDate = atual;
+    }
+
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", legalName=" + legalName + ", cnpj=" + cnpj + ", createDate=" + createDate
+				+ ", updateDate=" + updateDate + "]";
+	}
 }
