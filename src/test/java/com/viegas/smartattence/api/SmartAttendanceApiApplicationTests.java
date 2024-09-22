76b7a7063036da4cc491104f40c396f23bb84c82
Peter <peterviegas@outlook.com>
2024-09-22 13:16:35 +0100

    Config applicatrionTestProperties

--- src/test/java/com/viegas/smartattence/api/SmartAttendanceApiApplicationTests.java
+++ src/test/java/com/viegas/smartattence/api/SmartAttendanceApiApplicationTests.java
@@ -3,7 +3,12 @@ package com.viegas.smartattence.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 class SmartAttendanceApiApplicationTests {
 
 	@Test
